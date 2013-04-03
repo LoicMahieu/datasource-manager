134d1c99356a12ced1c5d9b765b5ef9a3791e219
Loïc Mahieu <mahieuloic@gmail.com>
2013-04-03 17:14:38 +0200

    Fix app
    Signed-off-by: Loïc Mahieu <mahieuloic@gmail.com>

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+
 var sys = require('sys');
-
 var exec = require('child_process').exec;
+var path = require('path');
 
-
-var child = exec("grunt dist-server", function(err, stdout, stderr) {
-	sys.print('stdout: ' + stdout);
-	sys.print('stderr: ' + stderr);
+var child = exec("grunt dist-server", {
+  cwd: __dirname
+}, function(err, stdout, stderr) {
 	if(err !== null) {
 		console.log('exec error: ' + err);
 	}
-})
+});
+
+child.stdout.on('data', function (d) {
+  sys.print(d);
+});
