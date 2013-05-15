744b79831cddcee3f02df3c7db8c11ae940e8c84
Younes Baddich <baddich5@gmail.com>
2013-05-15 14:16:32 +0200

    bug fix that were checking all checkboxes

--- api/routes/dbs.js
+++ api/routes/dbs.js
@@ -2,11 +2,37 @@
 var dbs = require('../lib/dbs');
 var send = require('./utils').send;
 
+var formatRows = function (rows) {
+  rows.forEach(formatRow);
+  return rows;
+};
+var formatRow = function (row) {
+  row.pooling = row.pooling === '1';
+
+  row.disable = row.disable === '1';
+  row.enable_clob = row.enable_clob === '1';
+  row.enable_blob = row.enable_blob === '1';
+
+  row.disable_autogenkeys = row.disable_autogenkeys === '1';
+
+  row.alter = row.alter === '1';
+  row.grant = row.grant === '1';
+  row.update = row.update === '1';
+  row.delete = row.delete === '1';
+  row.create = row.create === '1';
+  row.storedproc = row.storedproc === '1';
+  row.insert = row.insert === '1';
+  row.drop = row.drop === '1';
+  row.revoke = row.revoke === '1';
+  row.select = row.select === '1';
+};
+
 module.exports = function(ns, app) {
   app.get(ns + 'datasources', function(req, res) {
     dbs.list(function(err, rows) {
       if(err) throw err;
       
+      rows = formatRows(rows);
       send(res, rows);
     });
   });
