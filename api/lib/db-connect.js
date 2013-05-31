5c40b02046d9b94aaee19a789d5ca0c36866ab83
Loïc Mahieu <mahieuloic@gmail.com>
2013-05-31 12:20:57 +0200

    API database settings in config
    Signed-off-by: Loïc Mahieu <mahieuloic@gmail.com>

--- api/lib/db-connect.js
+++ api/lib/db-connect.js
@@ -1,11 +1,12 @@
 
+var config = require('config');
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host : 'localhost'
-    , database : 'igloo_cfadmin' 
-    , user : 'root'
-    , password : 'igloo'
+  host: config.db.host,
+  database: config.db.database,
+  user: config.db.user,
+  password: config.db.password
 });
 
 module.exports = connection;
